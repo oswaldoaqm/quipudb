a92aecdace857f6fa77ffe2fedb22614f704908e
oswaldoaqm <oswaldo.quispe@utec.edu.pe>
2026-09-05 23:31:19 -0500

    chore(build): agregar estructura y dependencias de la capa python

---
+++ engine/__init__.py
@@ -0,0 +1,5 @@
+"""Capa Python de QuipuDB: parser SQL, transacciones, planner y API REST.
+
+El motor de almacenamiento e indices vive en core/ (C++) y se importa a traves
+del modulo nativo `quipudb_native` generado por bindings/.
+"""
